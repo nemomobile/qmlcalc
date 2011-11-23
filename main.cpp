22d20351c9ae84384bf7e5c92cb6b31054300aa1
Robin Burchell <robin+git@viroteck.net>
2011-11-23 17:51:17 +0100

    hopefully omit white flashes

--- main.cpp
+++ main.cpp
@@ -10,7 +10,10 @@ int main(int argc, char **argv)
     QObject::connect(view.engine(), SIGNAL(quit()), &a, SLOT(quit()));
     view.setSource(QUrl("qrc:/qml/main.qml"));
     view.setResizeMode(QDeclarativeView::SizeRootObjectToView);
-//    view.setViewport(new QGLWidget);
     view.showFullScreen();
+    view.setAttribute(Qt::WA_OpaquePaintEvent);
+    view.setAttribute(Qt::WA_NoSystemBackground);
+    view.viewport()->setAttribute(Qt::WA_OpaquePaintEvent);
+    view.viewport()->setAttribute(Qt::WA_NoSystemBackground);
     return a.exec();
 }
