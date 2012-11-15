5eb580ace65bf86f8b29dd813078c75263838ed6
John Brooks <john.brooks@dereferenced.net>
2012-11-15 11:17:06 -0700

    Add qdeclarative-boostable support

--- main.cpp
+++ main.cpp
@@ -1,19 +1,28 @@
 #include <QDeclarativeView>
 #include <QDeclarativeEngine>
 #include <QApplication>
-#include <QGLWidget>
 
-int main(int argc, char **argv)
+#ifdef HAS_BOOSTER
+#include <MDeclarativeCache>
+#endif
+
+Q_DECL_EXPORT int main(int argc, char **argv)
 {
-    QApplication a(argc, argv);
-    QDeclarativeView view;
-    QObject::connect(view.engine(), SIGNAL(quit()), &a, SLOT(quit()));
-    view.setSource(QUrl("qrc:/qml/main.qml"));
-    view.setResizeMode(QDeclarativeView::SizeRootObjectToView);
-    view.showFullScreen();
-    view.setAttribute(Qt::WA_OpaquePaintEvent);
-    view.setAttribute(Qt::WA_NoSystemBackground);
-    view.viewport()->setAttribute(Qt::WA_OpaquePaintEvent);
-    view.viewport()->setAttribute(Qt::WA_NoSystemBackground);
-    return a.exec();
+#ifdef HAS_BOOSTER
+    QScopedPointer<QApplication> a(MDeclarativeCache::qApplication(argc, argv));
+    QScopedPointer<QDeclarativeView> view(MDeclarativeCache::qDeclarativeView());
+#else
+    QScopedPointer<QApplication> a(new QApplication(argc, argv));
+    QScopedPointer<QDeclarativeView> view(new QDeclarativeView);
+#endif
+
+    QObject::connect(view->engine(), SIGNAL(quit()), a.data(), SLOT(quit()));
+    view->setAttribute(Qt::WA_OpaquePaintEvent);
+    view->setAttribute(Qt::WA_NoSystemBackground);
+    view->viewport()->setAttribute(Qt::WA_OpaquePaintEvent);
+    view->viewport()->setAttribute(Qt::WA_NoSystemBackground);
+
+    view->setSource(QUrl("qrc:/qml/main.qml"));
+    view->showFullScreen();
+    return a->exec();
 }
